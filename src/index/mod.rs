fb1dc9e36795e9f1596b31eefc60961eea6d2c22
Rainweic <861514706@qq.com>
2023-12-01 16:56:36 +0800

    [update]

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -3,7 +3,7 @@ pub mod btree;
 use crate::data::log_record::LogRecordPos;
 
 
-// 索引接口
+// 索引接口 index下的所有数据结构都需要实现该特征
 pub trait Indexer {
 
     fn put(&self, key: &Vec<u8>, pos: &LogRecordPos) -> bool;
